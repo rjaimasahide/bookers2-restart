517347777dcafcd67a9b0cfef341dc47f209e4ce
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-09-07 12:38:54 +0000

    start[ログイン機能実装]

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "successfully"
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "successfully"
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "successfully"
+    user_path(current_user.id)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+end
